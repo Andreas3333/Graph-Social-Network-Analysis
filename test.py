2287e93a0db32a60caac3458a1624b371297d01c
Andreas3333 <75769702+Andreas3333@users.noreply.github.com>
2022-11-19 13:11:10 -0500

    test dtwParallel test

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
 import pandas as pd
-#import dtw
-#from scipy import cluster as cl
-
+from dtwParallel import dtw_functions
 '''
 get all values of name column and store in series object,
 reduce series object to contain single instances of the name value called (name_arr),
@@ -10,6 +8,9 @@ use name_arr to select rows based on column
 create new dataframes containing only rows of unique 'name' attribute
 
 
+////////////////////////////////////////////
+Data cleaning code-
+
 df.drop(columns=["Unnamed: 0", "High", "Low", "Close", "Adj Close"], inplace=True)
 df.convert_dtypes(convert_string=True)
 print(df.info())
@@ -17,9 +18,8 @@ print(df.info())
 f = open("basic_stock_dataset.csv", "w")
 str = str(df)
 f.write(str)
+////////////////////////////////////////////
 '''
-
-
 
 df = pd.read_csv('final_Dataset.csv', sep=',', lineterminator='\n')
 
@@ -27,21 +27,42 @@ name_arr = df.get('Name') #get() returns pandas.Series obj, array containing all
 
 name_arr.drop_duplicates(inplace = True) #inplace ensures a new obj is not returned, rather duplicate values are removed on the passed obj 
 
-i = 0
-j = name_arr.size
-while j >= 0:
+'''
+for i in name_arr:
     print(i)
-    str_1 = name_arr[i].to_string()
-    print(str_1)
+
+    company = df[df['Name'] == i]
     
-    #company = df.query('Name == str_1', inplace=False) #inplace=False: do not make changes to original dataframe(default behavior)
-    
-    company = df[df['Name'] == str_1]
-    
-    f = open('datasets/%s.csv' % str_1, 'w')
+    f = open('datasets/%s.csv' % i, 'w')
     fill = company.to_string() #change df type obj to str, only string type data can be written to file
     f.write(fill)
     f.close()
-    j -= 1
-    i += 1 
+print('Done')
+
+class Input:
+    def __init__(self):
+        self.check_errors = False 
+        self.type_dtw = "i"
+        self.MTS = True
+        self.n_threads = -1
+        self.distance = "dtw"
+        self.visualization = False
+        self.output_file = True
+        self.DTW_to_kernel = False
+        self.sigma_kernel = 1
+
+input_obj = Input()
+'''
+for i in name_arr:
+    for j in name_arr + 1:
+        company_a = df[df['Name'] == i]
+        company_b = df[df['Name'] == j]
+
+        f = open('dist-sim-test/%s.csv' % i, 'w')
+        # API call. 
+        fill = dtw_functions.dtw_ind(company_a, company_b, 0, False)
+        fill.to_string()
+        f.write(fill)
+        f.close()
+
 print('Done')
